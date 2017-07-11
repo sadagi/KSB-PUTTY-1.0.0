75883a5ce3d241236cea9af7e23e62ac48a7e350
Sadagi <sadagi.chandan@gmail.com>
2017-07-11 10:38:25 +0530

    commit and push by dba branch

---
+++ src/sadagi/utilities/example/Example_2.java
@@ -0,0 +1,12 @@
+package sadagi.utilities.example;
+
+public class Example_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Amit Sir");
+
+	}
+
+}
